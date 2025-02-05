a648962ff94526c1c3df23ec92356ba76583df73
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-02-05 22:14:21 +0600

    To find the common largest prefix entire Trie

--- Trie_basic.cpp
+++ Trie_basic.cpp
@@ -1,41 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Trienode {
+struct Trienode
+{
     struct Trienode *children[26];
     bool isEndofWord;
+    int childCnt;
 };
 
-struct Trienode *getnode() {
+struct Trienode *getnode()
+{
     struct Trienode *newnode = new Trienode;
     newnode->isEndofWord = false;
+    newnode->childCnt = 0;
 
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 26; i++)
+    {
         newnode->children[i] = NULL;
     }
 
     return newnode;
 }
 
-void insert(struct Trienode *root, string key) {
+void insert(struct Trienode *root, string key)
+{
     struct Trienode *current = root;
 
-    for (int i = 0; i < key.length(); i++) {
+    for (int i = 0; i < key.length(); i++)
+    {
         int index = key[i] - 'a';
         if (current->children[index] == NULL)
+        {
             current->children[index] = getnode();
+            current->childCnt++;
+        }
 
         current = current->children[index];
     }
     current->isEndofWord = true;
 }
 
-void search(struct Trienode *root, string key) {
+void search(struct Trienode *root, string key)
+{
     struct Trienode *current = root;
 
-    for (int i = 0; i < key.length(); i++) {
+    for (int i = 0; i < key.length(); i++)
+    {
         int index = key[i] - 'a';
-        if (current->children[index] == NULL) {
+        if (current->children[index] == NULL)
+        {
             cout << key << ": is not found\n";
             return;
         }
@@ -49,23 +62,28 @@ void search(struct Trienode *root, string key) {
 }
 
 // Helper function to check if a node has any children
-bool isEmpty(Trienode* root) {
-    for (int i = 0; i < 26; i++) {
+bool isEmpty(Trienode *root)
+{
+    for (int i = 0; i < 26; i++)
+    {
         if (root->children[i] != NULL)
             return false;
     }
     return true;
 }
 
-void deleteWord(Trienode* &root, string key, int depth = 0) {
-    if (root==NULL)
+void deleteWord(Trienode *&root, string key, int depth = 0)
+{
+    if (root == NULL)
         return;
 
-    if (depth == key.size()) {
+    if (depth == key.size())
+    {
         if (root->isEndofWord)
             root->isEndofWord = false;
 
-        if (isEmpty(root)) {
+        if (isEmpty(root))
+        {
             delete root;
             root = NULL;
         }
@@ -75,31 +93,56 @@ void deleteWord(Trienode* &root, string key, int depth = 0) {
     int index = key[depth] - 'a';
     deleteWord(root->children[index], key, depth + 1);
 
-    if (isEmpty(root) && !root->isEndofWord) {
+    if (isEmpty(root) && !root->isEndofWord)
+    {
         delete root;
         root = NULL;
     }
 }
 
-int main() {
-    string key[] = {"thanos", "apu", "lb", "a", "blackpanther"};
+string LCP(struct Trienode *root)
+{
+    string prefix = "";
+    struct Trienode *current = root;
+
+    while (current && current->childCnt == 1 && !current->isEndofWord)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (current->children[i] != NULL){
+                prefix+=(char)(i+'a');
+                current=current->children[i];
+                break;
+            }
+        }
+    }
+    return prefix;
+}
+
+int main()
+{
+    string key[] = {"coding", "codezen", "codingninja", "coders"};
 
     int n = sizeof(key) / sizeof(key[0]);
 
     struct Trienode *root = getnode();
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         insert(root, key[i]);
     }
 
-    search(root, "thanos");
+    search(root, "coding");
     search(root, "lb");
-    search(root, "blackpanther");
+    search(root, "codezen");
     search(root, "furie");
-    search(root, "bccd");
+    search(root, "coders");
+
+    cout << "\nThe common largest prefix is:\n";
+    cout << LCP(root) << "\n";
 
     cout << "\nDeleting 'thanos' from Trie...\n";
-    deleteWord(root, "thanos");
+    deleteWord(root, "coders");
 
-    search(root, "thanos"); 
+    search(root, "coders");
 }
