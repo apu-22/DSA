7d9e6319e4040dc64326597c65619100e5f15d27
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-02-16 11:38:37 +0600

    It is a basic code of directed graph.If i comment on graph[v][u], then undirected graph will convert into directed graph.

--- graphRepresentation.cpp
+++ graphRepresentation.cpp
@@ -1,11 +1,11 @@
-#include<bits\stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 void adjacencyMatrix(int nodes,int edges){
     vector<vector<int>>graph(nodes+1,vector<int>(nodes+1,0));
     for(int i=0;i<edges;i++){
         int u,v; cin>>u>>v;
         graph[u][v]=1;
-        graph[v][u]=1;
+        // graph[v][u]=1;
     }
 
     cout<<"Adjacency matrix of graph\n";
@@ -22,7 +22,7 @@ void adjacencyList(int nodes,int edges){
     for(int i=0;i<edges;i++){
         int u,v; cin>>u>>v;
         graph[u].push_back(v);
-        graph[v].push_back(u);
+        // graph[v].push_back(u);
     }
     cout<<"Adjacency List of graph\n";
     for(int i=1;i<=nodes;i++){
