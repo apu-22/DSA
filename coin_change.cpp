c2be87aca93d3a23f9cc4e66b6ba77cf39d9162c
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-07-17 19:36:24 +0600

    Coin change Dynamic problem using 2D matrix

---
+++ coin_change.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> c = {1, 3, 5};
+    int s = c.size();
+    cout << c[2];
+    int sum = 8;
+    int mt[s + 1][sum + 1];
+
+    for (int i = 0; i <= s; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                mt[i][j] = 1;
+            }
+            else if (i == 0)
+            {
+                mt[i][j] = 0; //// No way to make positive sum with 0 coins
+            }
+            else if (j == 0)
+            {
+                mt[i][j] = 1; // Only 1 way to make sum 0 — use no coins
+            }
+            else
+            {
+                if (c[i - 1] > j)
+                {
+                    mt[i][j] = mt[i - 1][j];
+                }
+                else
+                {
+                    mt[i][j] = mt[i - 1][j] + mt[i][j - c[i - 1]];
+                }
+            }
+        }
+    }
+    cout << "the total number of ways is: " << mt[s][sum] << "\n";
+}
